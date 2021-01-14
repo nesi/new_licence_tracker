942bea612ea272f52afd8cdf6c200121f61bc54a
Callum Walley <callum.walley@nesi.org.nz>
2021-01-15 12:17:18 +1300

    some common sense checks

--- main.py
+++ main.py
@@ -42,18 +42,26 @@ class LicenceCollector():
 
 # TODO Some validation
 def validate():
+    # List of valid promethius 'jobs'
+    prom_job_names = list(name["job_name"] for name in conf_prom["scrape_configs"])
+    for server in licences:
+        if server["server_prom_job"] not in prom_job_names:
+                log.warning(f"'{server['server_prom_job']}' is not a valid promethuis job. See '{conf['promethius_conf']}'")
+      
+  #check(licence)
     return
 
 
 if __name__ == '__main__':
     # Common sense checks.
-    if utils.run_cmd("whoami") != conf["user"]:
+    if utils.run_cmd("whoami").strip() != conf["user"]:
         log.warning(f"Run as {conf['user']} for read access on licences.")
-
     # Load licences, and defaults.
     licences = utils.load_licences(
         conf["licence_conf"], conf["default_licence_conf"]) 
     
+    validate()
+
     for prom_job in conf_prom["scrape_configs"]:
         # Get list of all licences using this frequency.
         scrape_interval = prom_job["scrape_interval"] if ("scrape_interval" in prom_job) else conf_prom["global"]["scrape_interval"]
@@ -66,10 +74,9 @@ if __name__ == '__main__':
             # Create registry and start server.
             registry_for_interval = CollectorRegistry(auto_describe=True)
             [host, port] = prom_job["static_configs"][0]["targets"][0].split(":")
+            log.info(f"Starting polling {len(licences_on_job)} servers at {scrape_interval} interval on Promethius port {port}")
             start_http_server(int(port), host, registry_for_interval)
             registry_for_interval.register(LicenceCollector(licences_on_job))
-            log.info(
-                f"Starting polling {len(licences_on_job)} servers at {scrape_interval} interval on Promethius port {port}")
 
         except Exception as e:
             log.error(f"Could not start promethius server {e}")
